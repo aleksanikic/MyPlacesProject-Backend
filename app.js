10d7a5667f20d96b3646a6e5d33322fd52e0c4d0
aleksa nikic~ <aleksanikic88@gmail.com>
2026-06-01 18:27:43 +0300

    fix

--- app.js
+++ app.js
@@ -41,6 +41,22 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+    console.log("ERROR CODE:", error.code);
+    console.log("ERROR MESSAGE:", error.message);
+    console.log("FULL ERROR:", error);
+    
+    if (req.file) {
+        fs.unlink(req.file.path, (err) => console.log(err));
+    }
+
+    if (res.headersSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message } || "An unknown error occurd");
+});
+
+app.use((error, req, res, next) => {
     if (req.file) {
         fs.unlink(req.file.path, (err) => console.log(err));
     }
