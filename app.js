1f95abf6c1d6ce951190a0d6600f14963c14e914
aleksa nikic~ <aleksanikic88@gmail.com>
2026-06-01 17:52:56 +0300

    fix CORS preflight

--- app.js
+++ app.js
@@ -15,9 +15,14 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
         "Access-Control-Allow-Headers",
-        "Origin,X-Requested-With, Content-Type,Accept,Authorization",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization",
     );
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, OPTIONS");
+
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
 
     next();
 });
